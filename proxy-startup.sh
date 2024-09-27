4dfa1641a3d8668f0e44afd0b0637dcf227441d3
PhoenixPink <141168667+PhoenixPink@users.noreply.github.com>
2024-09-27 13:09:34 +0700

    Update proxy-startup.sh

--- proxy-startup.sh
+++ proxy-startup.sh
@@ -190,12 +190,10 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal IP = ${IP4}. IP6 = ${IP6}"
 echo "How many proxy do you want to create? Example 500"
 read COUNT >$WORKDIR/last_port.txt
-COUNT="$WORKDIR/last_port.txt"
-COUNTNUM=$(${COUNT})
-echo "$COUNTNUM"
+echo "$(<$WORKDIR/last_port.txt)"
 
 FIRST_PORT=27081
-LAST_PORT=$((($FIRST_PORT-1) + $COUNTNUM))
+LAST_PORT=$((($FIRST_PORT-1) + $(<$WORKDIR/last_port.txt)))
 #LAST_PORT=$((($FIRST_PORT-1) + 5))
 
 gen_ipv6 >$WORKDIR/ipv6.txt
